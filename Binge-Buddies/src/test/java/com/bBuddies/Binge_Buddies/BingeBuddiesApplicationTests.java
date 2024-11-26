5a9170c45f4de9bc01f000b4cde29a1f240e042f
anshu <anshu@PagalHunk>
2024-11-26 19:58:59 +0530

    initial project push

---
+++ Binge-Buddies/src/test/java/com/bBuddies/Binge_Buddies/BingeBuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bBuddies.Binge_Buddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingeBuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
